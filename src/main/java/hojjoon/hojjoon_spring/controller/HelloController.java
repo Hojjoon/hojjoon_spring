abcd5c4073e53d7e1a20305cb5b482555ad4dcde
Hojjoon <60314127+Hojjoon@users.noreply.github.com>
2022-08-12 18:34:22 +0900

    Second Commit

--- src/main/java/hojjoon/hojjoon_spring/controller/HelloController.java
+++ src/main/java/hojjoon/hojjoon_spring/controller/HelloController.java
@@ -4,8 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+//abc
 public class HelloController {
     @GetMapping("hello")
     public String hello(Model model){
@@ -16,5 +18,28 @@ public class HelloController {
     public String helloMvc(@RequestParam(name = "name", required = true) String name, Model model){
         model.addAttribute("name", name);
         return "hello-template";
+    }
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello" + name; //  "hello spring"
+    }
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
---
+++ src/main/java/hojjoon/hojjoon_spring/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package hojjoon.hojjoon_spring.repository;
+
+import hojjoon.hojjoon_spring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
