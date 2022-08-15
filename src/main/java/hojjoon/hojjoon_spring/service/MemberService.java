5fbc64b28bb57b0e6b581172ea2d7e391fc7c3c6
Hojjoon <60314127+Hojjoon@users.noreply.github.com>
2022-08-15 16:39:38 +0900

    Third Commit

---
+++ src/main/java/hojjoon/hojjoon_spring/service/MemberService.java
@@ -0,0 +1,39 @@
+package hojjoon.hojjoon_spring.service;
+
+import hojjoon.hojjoon_spring.domain.Member;
+import hojjoon.hojjoon_spring.repository.MemberRepository;
+import hojjoon.hojjoon_spring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+
+    /*
+    *  회원가입
+    **/
+    public long join(Member member){
+        //같은 이름이 있는 중복회원 x
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member){
+        memberRepository.findByName(member.getName())
+                .ifPresent(m-> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+    public Optional<Member> findOne(long memberId){
+        return memberRepository.findById(memberId);
+    }
+
+
+}
