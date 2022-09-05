7d98cfac4a498ba43801dd98040cf82958b3859e
Hojjoon <60314127+Hojjoon@users.noreply.github.com>
2022-09-05 13:46:59 +0900

    2022/9/05 Commit 1.홈화면 2.회원가입 및 회원 조회 기능 추가

--- src/main/java/hojjoon/hojjoon_spring/SpringConfig.java
+++ src/main/java/hojjoon/hojjoon_spring/SpringConfig.java
@@ -9,13 +9,10 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringConfig {
 
-    @Bean
     public MemberService memberService(){
         return new MemberService(memberRepository());
     }
-    @Bean
     public MemberRepository memberRepository(){
         return new MemoryMemberRepository();
     }
-
 }
